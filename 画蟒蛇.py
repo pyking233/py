1fbab698dac18426f259f139a586cde0950070ad
fhb <fhb122089@gmail.com>
2021-10-06 23:51:46 +0800

    first commit

---
+++ 画蟒蛇.py
@@ -0,0 +1,16 @@
+import turtle as t
+t.setup(650, 350)
+t.penup()
+t.fd(-250)
+t.pendown()
+t.pensize(25)
+t.pencolor('gold')
+t.seth(-40)
+for i in range(4):
+    t.circle(40, 80)
+    t.circle(-40, 80)
+t.circle(40, 80/2)
+t.fd(40)
+t.circle(16, 180)
+t.fd(40 * 2/3)
+t.done()
---
+++ 练习.py
@@ -0,0 +1,9 @@
+Str = 'sdhjfkasjhfDKafasdHFJGJFYUIHJHDKASJH'
+print(Str.upper())
+print(Str.lower())
+print(Str.split('j'))
+print(Str.count('as'))
+print(Str.center(30, ':'))
+print(Str.replace('j', 'sdf'))
+print(Str.strip('sH'))
+print('/'.join(Str))
